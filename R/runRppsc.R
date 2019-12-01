89c2a873e5f650425b4321af4a082f7e1739623b
jiayanwang <jiayan.wang@mail.utoronto.com>
2019-11-30 20:21:27 -0500

    adding script to run shiny app

--- R/runRppsc.R
+++ R/runRppsc.R
@@ -1,8 +1,8 @@
 
 #run shiny app
 runRppsc <- function() {
-  appDir <- system.file("shiny-scripts",
-                        package = "TestingPackage")
+  appDir <- system.file("shiny-scripts","Rppsc",
+                        package = "Rppsc")
   shiny::runApp(appDir, display.mode = "normal")
   return()
 }
