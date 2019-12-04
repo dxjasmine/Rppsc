6f5fb8707dd1708c941d0899a86340d885520ab3
jiayanwang <jiayan.wang@mail.utoronto.com>
2019-12-04 14:11:54 -0500

    fix function bug: file and rbind

--- R/plotCG.R
+++ R/plotCG.R
@@ -18,7 +18,7 @@
 #' This function returns a composition graph given the attribute type.
 #'
 #' @examples
-#' filepath = "./data/proSeq.rda"
+#' filepath = "./data/protein_sequence.rda"
 #' p <- plotCG(filepath,type = 1) # default type is to show hydrophobicity composition
 #'
 #'
@@ -28,16 +28,18 @@
 #' @import utils
 
 library(utils)
+data("proSeq")
 
 plotCG <- function(file = "",type = 1) {
+
   #prepare data: from composition double to table
   if(!(type<6 & type>0 & type %% 1== 0)) {
     stop("Invalid value. Should be integer between 1-5")
   }
-#  if(!(file_test("-f",file))){
-#    stop("Invalid file path.")
-#    return()
-#  }
+  #  if(!(file_test("-f",file))){
+  #    stop("Invalid file path.")
+  #    return()
+  #  }
   if (!require("ggplot2")) {
     install.packages("ggplot2")
     library(ggplot2)
@@ -50,33 +52,36 @@ plotCG <- function(file = "",type = 1) {
   if (file == ""){
 
   }
-  proSeq = data.frame(lapply(proSeq, as.character), stringsAsFactors=FALSE)
-  tsize = nrow(proSeq)
+  data("proSeq")
+
+  protein_sequence = data.frame(lapply(proSeq, as.character), stringsAsFactors=FALSE)
+  pdbid_set = protein_sequence$pdb
+  tsize = nrow(protein_sequence)
   pname = NULL
-  pdata = NULL
+  pdata = data.frame()
   category = NULL
 
 
   for(i in 1:tsize ) {
 
     if(is.null(pname)){
-      pname = proSeq[i,1]
-      pdata = data.matrix(extractCTDC (proSeq[i,2]))
-      hydro = data.matrix(extractCTDC (proSeq[i,2]))[1:3,1]
-      vdm = data.matrix(extractCTDC (proSeq[i,2]))[4:6,1]
-      pol = data.matrix(extractCTDC (proSeq[i,2]))[7:9,1]
-      polabil =data.matrix(extractCTDC (proSeq[i,2]))[10:12,1]
-      desol = data.matrix(extractCTDC (proSeq[i,2]))[19:21,1]
+      pname = protein_sequence[i,1]
+      pdata = data.matrix(extractCTDC (protein_sequence[i,2]))
+      hydro = data.matrix(extractCTDC (protein_sequence[i,2]))[1:3,1]
+      vdm = data.matrix(extractCTDC (protein_sequence[i,2]))[4:6,1]
+      pol = data.matrix(extractCTDC (protein_sequence[i,2]))[7:9,1]
+      polabil =data.matrix(extractCTDC (protein_sequence[i,2]))[10:12,1]
+      desol = data.matrix(extractCTDC (protein_sequence[i,2]))[19:21,1]
 
 
     }else{
-      pname = rbind(pname, proSeq[i,1],row.names = NULL)
-      pdata = cbind(pdata, data.matrix(extractCTDC (proSeq[i,2])),row.names = NULL)
-      hydro = cbind(hydro, data.matrix(extractCTDC (proSeq[i,2]))[1:3,1],row.names = NULL)
-      vdm = cbind(vdm, data.matrix(extractCTDC (proSeq[i,2]))[4:6,1],row.names = NULL)
-      pol =cbind(pol, data.matrix(extractCTDC (proSeq[i,2]))[7:9,1],row.names = NULL)
-      polabil =cbind(polabil, data.matrix(extractCTDC (proSeq[i,2]))[10:12,1],row.names = NULL)
-      desol = cbind(desol, data.matrix(extractCTDC (proSeq[i,2]))[19:21,1],row.names = NULL)
+      pname = rbind(pname, protein_sequence[i,1],row.names = NULL)
+      pdata = cbind(pdata, data.matrix(extractCTDC (protein_sequence[i,2])),row.names = NULL)
+      hydro = cbind(hydro, data.matrix(extractCTDC (protein_sequence[i,2]))[1:3,1],row.names = NULL)
+      vdm = cbind(vdm, data.matrix(extractCTDC (protein_sequence[i,2]))[4:6,1],row.names = NULL)
+      pol =cbind(pol, data.matrix(extractCTDC (protein_sequence[i,2]))[7:9,1],row.names = NULL)
+      polabil =cbind(polabil, data.matrix(extractCTDC (protein_sequence[i,2]))[10:12,1],row.names = NULL)
+      desol = cbind(desol, data.matrix(extractCTDC (protein_sequence[i,2]))[19:21,1],row.names = NULL)
     }
 
   }
@@ -95,8 +100,9 @@ plotCG <- function(file = "",type = 1) {
 
   supp=rep(pname, each=3)
   len=c(len_type)
-  dataf <- data.frame(supp=rep(c(pname), each=3),category=rep(attrname, tsize),len=hydro)
-    p = ggplot(data=dataf, aes(x=supp, y=len, fill=category)) +
+  dataf <- data.frame(supp=rep(c(pname), each=3),category=rep(attrname, tsize),len=hydro, row.names =NULL)
+
+  p = ggplot(data=dataf, aes(x=supp, y=len, fill=category)) +
     geom_bar(stat="identity") +
 
     ggtitle("Protein Sequence composition")
@@ -105,5 +111,4 @@ plotCG <- function(file = "",type = 1) {
 
 
 }
-
 
