114912bb4f2e79b568fc12a220b57964301a9de1
jiayanwang <jiayan.wang@mail.utoronto.com>
2019-12-04 23:24:52 -0500

    updated namespace

--- R/runRppsc.R
+++ R/runRppsc.R
@@ -1,5 +1,19 @@
-
-#run shiny app
+#' launch Rppsc shiny app
+#'
+#' A function launches the Rppsc Shiny app that allows the user to use the features
+#' of Rppsc package in an interactive way.
+#'
+#'
+#' @return null
+#'
+#' @examples
+#' \dontrun{
+#'   runRppsc()
+#' }
+#'
+#'
+#' @export
+#' @importFrom shiny runApp
 runRppsc <- function() {
   appDir <- system.file("shiny-scripts","Rppsc",
                         package = "Rppsc")
